6d80ab772f5a7de9c6e3791c940b4226c947164d
Daniel Martin <etd-github@nomejortu.com>
2014-11-06 17:05:40 +0100

    Include a Configuration object

--- lib/dradis/plugins/vulndb.rb
+++ lib/dradis/plugins/vulndb.rb
@@ -1,6 +1,10 @@
 module Dradis
   module Plugins
     module Vulndb
+      class Configuration < Core::Configurator
+        configure namespace: 'vulndb'
+        setting :hq_rest_url, default: 'https://youremail%40emaildomain.com:password@user.vulndbhq.com'
+      end
     end
   end
 end
